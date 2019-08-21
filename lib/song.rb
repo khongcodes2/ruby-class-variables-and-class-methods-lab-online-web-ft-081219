fd94991dd38bc4e8db35aacca3dbf4dd2dbcceb6
K Hong <albtrossfacefall@gmail.com>
2019-08-21 21:58:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,17 @@
 class Song
+  attr_accessor :name, :artist, :genre
+  @@count=0
+  @@artists=[]
+  @@genre=[]
+  
+  def initialize(name,artist,genre)
+    @@count +=1
+    @name=name
+    @artist=artist
+    @@artist.push(artist)||=
+    @genre=genre
+  end
+
+  def artists
   
 end
